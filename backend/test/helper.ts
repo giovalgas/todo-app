3358c3ade3705f9c185f585ec85553cb879f4436
Giovani Valgas <giovalgascom@gmail.com>
2024-03-28 22:00:37 -0300

    refactor: run prettier

--- backend/test/helper.ts
+++ backend/test/helper.ts
@@ -4,34 +4,31 @@ import * as path from 'path'
 import * as test from 'node:test'
 
 export interface TestContext {
-  after: typeof test.after
+    after: typeof test.after
 }
 
 const AppPath = path.join(__dirname, '..', 'src', 'app.ts')
 
 // Fill in this config with all the configurations
 // needed for testing the application
-async function config () {
-  return {}
+async function config() {
+    return {}
 }
 
 // Automatically build and tear down our instance
-async function build (t: TestContext) {
-  // you can set all the options supported by the fastify CLI command
-  const argv = [AppPath]
+async function build(t: TestContext) {
+    // you can set all the options supported by the fastify CLI command
+    const argv = [AppPath]
 
-  // fastify-plugin ensures that all decorators
-  // are exposed for testing purposes, this is
-  // different from the production setup
-  const app = await helper.build(argv, await config())
+    // fastify-plugin ensures that all decorators
+    // are exposed for testing purposes, this is
+    // different from the production setup
+    const app = await helper.build(argv, await config())
 
-  // Tear down our app after we are done
-  t.after(() => void app.close())
+    // Tear down our app after we are done
+    t.after(() => void app.close())
 
-  return app
+    return app
 }
 
-export {
-  config,
-  build
-}
+export { config, build }
--- backend/test/plugins/support.test.ts
+++ backend/test/plugins/support.test.ts
@@ -5,9 +5,9 @@ import Fastify from 'fastify'
 import Support from '../../src/plugins/support'
 
 test('support works standalone', async (t) => {
-  const fastify = Fastify()
-  void fastify.register(Support)
-  await fastify.ready()
+    const fastify = Fastify()
+    void fastify.register(Support)
+    await fastify.ready()
 
-  assert.equal(fastify.someSupport(), 'hugs')
+    assert.equal(fastify.someSupport(), 'hugs')
 })
